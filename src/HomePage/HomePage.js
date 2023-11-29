751cada1d178f51996420a28e97e1e9fb31e8e6d
Ian Czerkis <czerkisi@msoe.edu>
2023-11-29 15:41:13 -0600

    fix routes

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -10,25 +10,25 @@ const HomePage = () => {
             <nav className="navbar navbar-dark bg-dark">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/#/stats">Statistics</a>
+                        <a className="nav-link" href="/raider-wagers/#/stats">Statistics</a>
                     </li>
                 </ul>
                 
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/#/exchange">Exchanges</a>
+                        <a className="nav-link" href="/raider-wagers/#/exchange">Exchanges</a>
                     </li>
                 </ul>
 
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/#/events">Events</a>
+                        <a className="nav-link" href="/raider-wagers/#/events">Events</a>
                     </li>
                 </ul>
 
                 <ul className="navbar-nav mr-auto">
                     <li className="navbar-nav mr-auto">
-                        <a className="nav-link" href="/#/wager">Wagers</a>
+                        <a className="nav-link" href="/raider-wagers/#/wager">Wagers</a>
                     </li>
                 </ul>
             </nav>
