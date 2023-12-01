1de814ed7e240116470cffa9b6ee2b73fb5b3e12
nieberlen <nieberlen@msoe.edu>
2023-11-30 18:05:43 -0600

    Added "Reprompt to use last amount feature" into placing bet selection. In the event there are no bets the button is disabled and will update to be enabled once the first bet is placed.

--- src/WagerPage/WagerPage.js
+++ src/WagerPage/WagerPage.js
@@ -8,10 +8,22 @@ const WagerPage = () => {
     const [selectedBet, setSelectedBet] = useState(null);
     const [isOverlayVisible, setIsOverlayVisible] = useState(false);
     const [betAmount, setBetAmount] = useState(0);
+    let disableButton = true
+    if (JSON.parse(localStorage.getItem('bets')) != null) {
+        disableButton = false
+    }
+    const [isButtonDisabled, setIsButtonDisabled] = useState(disableButton);
+
+
+
+
 
     const { search } = useLocation();
     const queryParams = new URLSearchParams(search);
     const gameID = parseInt(queryParams.get("gameID"));
+
+
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,6 +33,7 @@ const WagerPage = () => {
             }
         };
         fetchData();
+
     }, [gameID]);
 
     const handleBetClick = (bet) => {
@@ -49,10 +62,25 @@ const WagerPage = () => {
             setSelectedBet(null);
             setIsOverlayVisible(false);
             setBetAmount(0);
+            setIsButtonDisabled(false);
         } else{
             console.log('not enough dabloons for bet');
         }
     };
+
+    const handleReuseLastBetAmount = () => {
+        const existingBets = JSON.parse(localStorage.getItem('bets'));
+        if (existingBets != null) {
+
+
+            const lastBet = existingBets[existingBets.length - 1].amount
+            const inputButton = document.getElementById("inputButton")
+            inputButton.value = lastBet
+        } else {
+            setIsButtonDisabled(true);
+            console.log("Something happened to the bets in local storage")
+        }
+    }
 
     return (
         <div>
@@ -114,10 +142,12 @@ const WagerPage = () => {
                             <h3>Enter Amount to Bet</h3>
                             <input
                                 type="number"
+                                id = "inputButton"
                                 value={betAmount}
                                 onChange={(e) => setBetAmount(e.target.value)}
                             />
                             <button onClick={handleConfirmBet}>Confirm Bet</button>
+                            <button onClick={handleReuseLastBetAmount} disabled={isButtonDisabled}>Reuse last bet amount</button>
                         </div>
                     </div>
                 </div>
