4e7bcb99dfae5e60a747c477e364f0321fc017d2
mirandae776 <mirandae@msoe.edu>
2023-12-01 19:03:37 -0600

    aded date to bets needs some reformatting

--- src/BetHistory/BetPage.js
+++ src/BetHistory/BetPage.js
@@ -4,21 +4,34 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BetTable = () => {
-    // State to store the retrieved data
     const [bets, setBets] = useState([]);
 
-    // useEffect to run once when the component mounts
     useEffect(() => {
-        // Get data from local storage using the key "bets"
         const storedBets = localStorage.getItem('bets');
-
-        // Parse the JSON data and update the state
         if (storedBets) {
             setBets(JSON.parse(storedBets));
         }
     }, []);
 
-    // Render a table with Bootstrap styling or a 'no results' message
+    const handleDelete = (uid, gameDate) => {
+        // Filter out the bet with the specified UUID
+        console.log(uid)
+        const updatedBets = bets.filter((bet) => bet.uid !== uid);
+        const now = new Date();
+        const currentDate = now.toLocaleDateString();
+
+        // Check if the current date is past the game date
+        if (new Date(currentDate) > new Date(gameDate)) {
+            console.log('No, cannot delete. Game date has already passed.');
+            return; // Exit the function without updating state or local storage
+        }
+
+        // Update the state and local storage with the modified data
+        setBets(updatedBets);
+        localStorage.setItem('bets', JSON.stringify(updatedBets));
+    };
+
+
     return (
         <div>
             <a className="nav-link" href="/raider-wagers/home#/home">Home</a>
@@ -27,6 +40,7 @@ const BetTable = () => {
                 <Table striped bordered hover>
                     <thead>
                     <tr>
+                        <th>Game Date</th>
                         <th>Amount</th>
                         <th>Odds</th>
                         <th>Prop</th>
@@ -37,12 +51,15 @@ const BetTable = () => {
                     <tbody>
                     {bets.map((bet, index) => (
                         <tr key={index}>
+                            <td>{bet.date}</td>
                             <td>{bet.amount}</td>
                             <td>{bet.odds}</td>
                             <td>{bet.prop}</td>
                             <td>{bet.propName}</td>
                             <td>
-                                <Button variant="danger">Cancel</Button>
+                                <Button variant="danger" size="sm" onClick={() => handleDelete(bet.uid)}>
+                                    Cancel
+                                </Button>
                                 {''}
                             </td>
                         </tr>
