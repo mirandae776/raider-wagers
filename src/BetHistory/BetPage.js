5bae558d8af582a74b429fe7af666cbb16f388fd
mirandae776 <mirandae@msoe.edu>
2023-12-03 14:11:04 -0600

    Can remove bets only if they are in the future, also added the ability to sort bet table

--- src/BetHistory/BetPage.js
+++ src/BetHistory/BetPage.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'react-bootstrap';
 
 const BetTable = () => {
     const [bets, setBets] = useState([]);
+    const [selectedOption, setSelectedOption] = useState('recentlyPlaced');
 
     useEffect(() => {
         const storedBets = localStorage.getItem('bets');
@@ -15,60 +17,88 @@ const BetTable = () => {
 
     const handleDelete = (uid, gameDate) => {
         // Filter out the bet with the specified UUID
-        console.log(uid)
         const updatedBets = bets.filter((bet) => bet.uid !== uid);
-        const now = new Date();
-        const currentDate = now.toLocaleDateString();
-
-        // Check if the current date is past the game date
-        if (new Date(currentDate) > new Date(gameDate)) {
-            console.log('No, cannot delete. Game date has already passed.');
-            return; // Exit the function without updating state or local storage
-        }
 
         // Update the state and local storage with the modified data
         setBets(updatedBets);
         localStorage.setItem('bets', JSON.stringify(updatedBets));
     };
 
+    const handleDropdownChange = (selected) => {
+        setSelectedOption(selected);
+        localStorage.setItem('selectedOption', selected);
+    };
+
+    const renderTable = () => {
+        let sortedBets = [...bets];
+
+        if (selectedOption === 'gameDate') {
+            sortedBets = sortedBets.sort((a, b) => new Date(a.date) - new Date(b.date));
+        }
+
+        return (
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>Game Date</th>
+                    <th>Amount</th>
+                    <th>Odds</th>
+                    <th>Prop</th>
+                    <th>Prop Name</th>
+                    <th>Cancel</th>
+                </tr>
+                </thead>
+                <tbody>
+                {sortedBets.map((bet, index) => (
+                    <tr key={index}>
+                        <td>{bet.date}</td>
+                        <td>{bet.amount}</td>
+                        <td>{bet.odds}</td>
+                        <td>{bet.prop}</td>
+                        <td>{bet.propName}</td>
+                        <td>
+                            <Button
+                                variant="danger"
+                                size="sm"
+                                disabled={new Date() > new Date(bet.date)}
+                                onClick={() => handleDelete(bet.uid, bet.date)}
+                            >
+                                Cancel
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </Table>
+        );
+    };
 
     return (
-        <div>
-            <a className="nav-link" href="/raider-wagers/home#/home">Home</a>
+        <div className="ml-auto">
+            <Button>
+                <a className="nav-link" href="/raider-wagers/home#/home">
+                    Home
+                </a>
+            </Button>
+
             <h2>Bet History</h2>
-            {bets.length > 0 ? (
-                <Table striped bordered hover>
-                    <thead>
-                    <tr>
-                        <th>Game Date</th>
-                        <th>Amount</th>
-                        <th>Odds</th>
-                        <th>Prop</th>
-                        <th>Prop Name</th>
-                        <th>Cancel</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {bets.map((bet, index) => (
-                        <tr key={index}>
-                            <td>{bet.date}</td>
-                            <td>{bet.amount}</td>
-                            <td>{bet.odds}</td>
-                            <td>{bet.prop}</td>
-                            <td>{bet.propName}</td>
-                            <td>
-                                <Button variant="danger" size="sm" onClick={() => handleDelete(bet.uid)}>
-                                    Cancel
-                                </Button>
-                                {''}
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </Table>
-            ) : (
-                <p>When you start to place bets, you can find the history here</p>
-            )}
+
+            <div>
+                <Dropdown onSelect={handleDropdownChange}>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Sort By
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item eventKey="recentlyPlaced">Recently Placed</Dropdown.Item>
+                        <Dropdown.Item eventKey="gameDate">Game Date</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <span style={{ marginLeft: '10px' }}>Selected Option: {selectedOption}</span>
+            </div>
+
+            {bets.length > 0 ? renderTable() : <p>When you start to place bets, you can find the history here</p>}
         </div>
     );
 };
